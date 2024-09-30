1be383866fd68d98b25acb16882346bc8f23e9ba
dcicio <dcicio@gmail.com>
2024-09-30 15:58:01 -0400

    main.py now prints the word count to console

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ def main():
     # Relative filepath of the text of Frankstein, presuming running from ~/workspace/github.com/dcicio/bookbot
     path_to_file = "books/frankenstein.txt"
     
-    # Opening and reading the file into a variable
-    with open(path_to_file) as f:
-        file_contents = f.read()
+    file_contents = get_book_text(path_to_file)
     
-    # Printing the text to console
-    print(file_contents)
+    word_count = get_num_words(file_contents)
+
+    print(f"{word_count} words found in the document")
+    
+# Open and read the file into a variable
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+# Get the word count of a text string
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 main()
